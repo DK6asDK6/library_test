f4978b2492a07de676a1cccf6a9933b20560fa23
DK6asDK6 <kirillovdiitrij96@gmail.com>
2026-08-18 17:15:33 +0300

    fixed GET api/users request.

--- routes/users.js
+++ routes/users.js
@@ -61,7 +61,7 @@ router.get('/', async (req, res, next) => {
         const user = await User.findById(req.body._id);
 
         if (user != null && user.access === 2) {
-            const users = await User.find({}, {_id: 0, password: 0});
+            const users = await User.find();
             res.json(users);
         } else {
             return res.status(404).json({error: 'Access forbidden'});
